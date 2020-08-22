e1718cdf86a775ca3b727d6b9fe6d5e56ca6cc25
Venky <shrivyankateshj@gmail.com>
2020-08-21 23:28:06 -0400

    Initial commit of Core Java Project

---
+++ src/com/lv1/cj/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lv1.cj;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Venky!!");
+		System.out.print("Welcome to Java Programming Again!!!!");
+	}
+
+}
